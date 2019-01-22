80e701c9050ed02e2278d4bb8510ff498876b124
Christopher Zell <zelldon91@googlemail.com>
2019-01-22 14:22:44 +0100

    add leader election primitive

--- poc-atomix/poc-logstream/src/main/java/de/zell/Primitive.java
+++ poc-atomix/poc-logstream/src/main/java/de/zell/Primitive.java
@@ -1,26 +1,23 @@
 package de.zell;
 
-import io.atomix.core.map.DistributedMap;
-import io.atomix.core.map.DistributedMapBuilder;
-import java.io.File;
-import java.lang.reflect.Field;
-import java.lang.reflect.Modifier;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
-
 import de.zell.logstream.DistributedLogstream;
 import de.zell.logstream.DistributedLogstreamBuilder;
 import de.zell.logstream.DistributedLogstreamConfig;
 import de.zell.logstream.DistributedLogstreamType;
 import io.atomix.core.Atomix;
 import io.atomix.core.AtomixBuilder;
-import io.atomix.primitive.log.LogSession;
+import io.atomix.core.election.Leader;
+import io.atomix.core.election.LeaderElection;
+import io.atomix.core.election.Leadership;
+import io.atomix.core.election.LeadershipEvent;
 import io.atomix.protocols.raft.MultiRaftProtocol;
 import io.atomix.protocols.raft.ReadConsistency;
-import io.atomix.protocols.raft.partition.RaftCompactionConfig;
 import io.atomix.protocols.raft.partition.RaftPartitionGroup;
 import io.atomix.utils.net.Address;
+import java.io.File;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -36,7 +33,7 @@ public class Primitive extends Thread {
   private final int port;
   private final List<String> allMemberList;
   private final File memberFolder;
- private final Set<String> members;
+  private final Set<String> members;
 
   public Primitive(
       final File rootFolder, final String memberId, int port, final List<String> memberList) {
@@ -81,8 +78,8 @@ public class Primitive extends Thread {
 
     final RaftPartitionGroup raftPartitionGroup =
         RaftPartitionGroup.builder(partitionGroupName)
-            .withNumPartitions(3)
-            .withPartitionSize(3)
+            .withNumPartitions(allMemberList.size())
+            .withPartitionSize(allMemberList.size())
             .withMembers(allMemberList)
             .withDataDirectory(partitionGroupFolder)
             .withFlushOnCommit()
@@ -103,18 +100,69 @@ public class Primitive extends Thread {
     node.start().join();
     LOG.info("Member {} started.", memberId);
 
-      final MultiRaftProtocol multiRaftProtocol =
-          MultiRaftProtocol.builder(partitionGroupName)
-              .withReadConsistency(ReadConsistency.LINEARIZABLE)
-              .build();
+    final MultiRaftProtocol multiRaftProtocol =
+        MultiRaftProtocol.builder(partitionGroupName)
+            .withReadConsistency(ReadConsistency.LINEARIZABLE)
+            .build();
 
-      LOG.info("Build logstream primitive.");
-      // build custom primitive
-      node.<DistributedLogstreamBuilder, DistributedLogstreamConfig, DistributedLogstream>
-              primitiveBuilder("logstream", DistributedLogstreamType.instance())
-          .withProtocol(multiRaftProtocol)
-          .build();
+    LOG.info("Build logstream primitive.");
+    // build custom primitive
+    node.<DistributedLogstreamBuilder, DistributedLogstreamConfig, DistributedLogstream>
+            primitiveBuilder("logstream", DistributedLogstreamType.instance())
+        .withProtocol(multiRaftProtocol)
+        .build();
 
     LOG.info("Logstream primitive build.");
+
+    LOG.info("Create leader election primitive.");
+
+    final MultiRaftProtocol multiRaftOnSystem =
+        MultiRaftProtocol.builder(systemPartitionGroupName)
+            .withReadConsistency(ReadConsistency.LINEARIZABLE)
+            .build();
+
+    raftPartitionGroup
+        .getPartitions()
+        .forEach(
+            partition -> {
+              final String partitionString =
+                  new StringBuilder()
+                      .append(partition.id().group())
+                      .append("-")
+                      .append("partition")
+                      .append("-")
+                      .append(partition.id().id())
+                      .toString();
+              final LeaderElection<String> election =
+                  node.<String>leaderElectionBuilder(partitionString)
+                      .withProtocol(multiRaftOnSystem)
+                      .build();
+
+              LOG.info("Created leader election primitive {}.", partitionString);
+              election.addListener(this::onLeadershipEvent);
+
+              election.run(memberId);
+            });
+  }
+
+  private void onLeadershipEvent(LeadershipEvent<String> leadershipEvent) {
+    final Leader<String> newLeader = leadershipEvent.newLeadership().leader();
+    final Leadership<String> oldLeadership = leadershipEvent.oldLeadership();
+    final Leader<String> oldLeader = oldLeadership.leader();
+
+    final boolean leaderHasNotChanged = newLeader.equals(oldLeader);
+    if (leaderHasNotChanged) {
+      final boolean wasMemberBefore = oldLeadership.candidates().contains(memberId);
+      if (wasMemberBefore) {
+        return;
+      }
+    }
+
+    final String leaderId = newLeader.id();
+    if (leaderId.equalsIgnoreCase(memberId)) {
+      LOG.error("I'm {} and take the lead for partition {}", memberId, leadershipEvent.topic());
+    } else {
+      LOG.info("I'm {} and will simply follow partition {}", memberId, leadershipEvent.topic());
+    }
   }
 }
